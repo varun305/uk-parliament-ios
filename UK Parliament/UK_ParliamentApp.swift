2a81daba277b1a76436b321287b7dabd9b95d233
Varun Santhosh <varunsanthosh2002@gmail.com>
2023-12-21 10:32:14 +0000

    Initial Commit

---
+++ UK Parliament/UK_ParliamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UK_ParliamentApp.swift
+//  UK Parliament
+//
+//  Created by Varun Santhosh on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct UK_ParliamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
