ee8c5d1097fbb8a3ef0a738f2cac2ecfef9890a8
Varun Santhosh <varunsanthosh2002@gmail.com>
2024-02-01 07:30:14 +0000

    skeletion loading #7

--- UK Parliament/Views/Member/MemberDetail/MemberContact/MemberContactView.swift
+++ UK Parliament/Views/Member/MemberDetail/MemberContact/MemberContactView.swift
@@ -1,10 +1,51 @@
 import SwiftUI
+import SkeletonUI
 
 struct MemberContactView: View {
     @StateObject var viewModel = MemberContactViewModel()
     var member: Member
 
     var body: some View {
+        Group {
+            if viewModel.contacts.count > 0 {
+                scrollView
+            } else if viewModel.loading {
+                loadingView
+            } else {
+                Text("No data")
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+                    .italic()
+            }
+        }
+        .onAppear {
+            if let memberId = member.id {
+                viewModel.fetchContacts(for: memberId)
+            }
+        }
+        .navigationTitle("Contact details, \(member.nameDisplayAs ?? "")")
+    }
+
+    @ViewBuilder
+    var loadingView: some View {
+        List {
+            ForEach(0..<5) { _ in
+                Section("") {
+                    Text("")
+                        .skeleton(with: true)
+                        .frame(height: 10)
+                    Text("")
+                        .skeleton(with: true)
+                        .frame(height: 10)
+                }
+            }
+        }
+        .listStyle(.plain)
+        .environment(\.isScrollEnabled, false)
+    }
+
+    @ViewBuilder
+    var scrollView: some View {
         List {
             ForEach(viewModel.contacts) { contact in
                 Section(contact.type ?? "") {
@@ -31,15 +72,9 @@ struct MemberContactView: View {
                         }
                     }
                     .font(.footnote)
-                    .textSelection(.enabled)
                 }
             }
         }
-        .onAppear {
-            if let memberId = member.id {
-                viewModel.fetchContacts(for: memberId)
-            }
-        }
-        .navigationTitle("Contact details, \(member.nameDisplayAs ?? "")")
+        .listStyle(.grouped)
     }
 }
--- UK Parliament/Views/Member/MemberDetail/MemberContact/MemberContactViewModel.swift
+++ UK Parliament/Views/Member/MemberDetail/MemberContact/MemberContactViewModel.swift
@@ -1,13 +1,19 @@
 import Foundation
+import SwiftUI
 
 extension MemberContactView {
     @MainActor class MemberContactViewModel: ObservableObject {
         @Published var contacts: [MemberContact] = []
+        @Published var loading = false
 
         public func fetchContacts(for id: Int) {
+            loading = true
             MemberModel.shared.fetchMemberContacts(for: id) { result in
                 Task { @MainActor in
-                    self.contacts = result?.value ?? []
+                    withAnimation {
+                        self.contacts = result?.value ?? []
+                        self.loading = false
+                    }
                 }
             }
         }
