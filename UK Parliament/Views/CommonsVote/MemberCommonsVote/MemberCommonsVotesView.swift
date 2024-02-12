57f2afcbcf26f600d35096b8d5cbfbe19492bac8
Varun Santhosh <varunsanthosh2002@gmail.com>
2024-02-12 08:24:57 +0000

    member commons votes

--- UK Parliament/Views/CommonsVote/MemberCommonsVote/MemberCommonsVotesView.swift
+++ UK Parliament/Views/CommonsVote/MemberCommonsVote/MemberCommonsVotesView.swift
@@ -5,9 +5,9 @@ struct MemberCommonsVotesView: View {
 
     var body: some View {
         Group {
-            if viewModel.loading && viewModel.memberVotes.isEmpty {
+            if viewModel.loading && viewModel.items.isEmpty {
                 loadingView
-            } else if !viewModel.memberVotes.isEmpty {
+            } else if !viewModel.items.isEmpty {
                 scrollView
             } else {
                 NoDataView()
@@ -23,7 +23,7 @@ struct MemberCommonsVotesView: View {
         .navigationTitle("Commons votes, \(viewModel.member.nameDisplayAs ?? "")")
         .navigationBarTitleDisplayMode(.inline)
         .onAppear {
-            if viewModel.memberVotes.isEmpty {
+            if viewModel.items.isEmpty {
                 viewModel.nextData(reset: true)
             }
         }
@@ -43,7 +43,7 @@ struct MemberCommonsVotesView: View {
 
     @ViewBuilder
     var scrollView: some View {
-        List(viewModel.memberVotes) { memberVote in
+        List(viewModel.items) { memberVote in
             MemberCommonsVoteRow(memberVote: memberVote)
                 .ifLet(memberVote.publishedDivision) { view, commonsVote in
                     ContextAwareNavigationLink(value: .commonsVoteDetailView(vote: commonsVote)) {
@@ -56,7 +56,7 @@ struct MemberCommonsVotesView: View {
     }
 
     private func onScrollEnd(memberVote: MemberCommonsVote) {
-        if memberVote == viewModel.memberVotes.last {
+        if memberVote == viewModel.items.last {
             viewModel.nextData()
         }
     }
--- UK Parliament/Views/CommonsVote/MemberCommonsVote/MemberCommonsVotesViewModel.swift
+++ UK Parliament/Views/CommonsVote/MemberCommonsVote/MemberCommonsVotesViewModel.swift
@@ -2,52 +2,25 @@ import Foundation
 import SwiftUI
 import Combine
 
-@MainActor class MemberCommonsVotesViewModel: ObservableObject {
+@MainActor class MemberCommonsVotesViewModel: UnifiedListViewModel<MemberCommonsVote> {
     var member: Member
-    @Published var memberVotes: [MemberCommonsVote] = []
-    @Published var search = ""
-    @Published var loading = true
 
     init(member: Member) {
         self.member = member
-        addSearchSubscriber()
+        super.init()
     }
 
-    private var cancellables = Set<AnyCancellable>()
-    private func addSearchSubscriber() {
-        $search
-            .debounce(for: 0.3, scheduler: DispatchQueue.main)
-            .sink { [weak self] searchText in
-                self?.nextData(searchText: searchText, reset: true)
+    override internal func fetchNextData(search: String, reset: Bool, completion: @escaping ([MemberCommonsVote], Int) -> Void) {
+        if let id = member.id {
+            MemberVoteModel.shared.nextMemberCommonsData(memberId: id, search: search, reset: reset) { result in
+                completion(result ?? [], 0)
             }
-            .store(in: &cancellables)
+        } else {
+            completion([], 0)
+        }
     }
 
-    public func nextData(searchText: String? = nil, reset: Bool = false) {
-        let search = searchText ?? self.search
-        if !MemberVoteModel.shared.canGetNextCommonsData(search: search, reset: reset) {
-            return
-        }
-
-        if reset {
-            withAnimation {
-                loading = true
-                memberVotes = []
-            }
-        }
-        if let memberId = member.id {
-            MemberVoteModel.shared.nextMemberCommonsData(memberId: memberId, search: search, reset: reset) { result in
-                Task { @MainActor in
-                    withAnimation {
-                        if reset {
-                            self.memberVotes = result ?? []
-                            self.loading = false
-                        } else {
-                            self.memberVotes += result ?? []
-                        }
-                    }
-                }
-            }
-        }
+    override internal func canFetchNextData(search: String, reset: Bool) -> Bool {
+        return MemberVoteModel.shared.canGetNextCommonsData(search: search, reset: reset)
     }
 }
