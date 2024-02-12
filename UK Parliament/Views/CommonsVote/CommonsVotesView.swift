2f1b1290f91d75fca9707d4a9242a64b475f9210
Varun Santhosh <varunsanthosh2002@gmail.com>
2024-02-12 19:54:58 +0000

    commons votes

--- UK Parliament/Views/CommonsVote/CommonsVotesView.swift
+++ UK Parliament/Views/CommonsVote/CommonsVotesView.swift
@@ -2,59 +2,19 @@ import SwiftUI
 
 struct CommonsVotesView: View {
     @StateObject var viewModel = CommonsVotesViewModel()
-
     var body: some View {
-        Group {
-            if viewModel.loading && viewModel.items.isEmpty {
-                loadingView
-            } else if !viewModel.items.isEmpty {
-                scrollView
-            } else {
-                NoDataView()
-            }
-        }
-        .searchable(text: $viewModel.search, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a commons vote")
-        .onChange(of: viewModel.search) { _, new in
-            if new.isEmpty {
-                viewModel.search = ""
-                viewModel.nextData(reset: true)
-            }
-        }
-        .navigationTitle("Commons votes")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-            if viewModel.items.isEmpty {
-                viewModel.nextData(reset: true)
-            }
-        }
-        .toolbarBackground(Color.commons.opacity(0.1))
-    }
-
-    @ViewBuilder
-    var loadingView: some View {
-        List(0..<20) { _ in
-            DummyNavigationLink {
+        UnifiedListView(
+            viewModel: viewModel,
+            rowView: { commonsVote in
+                ContextAwareNavigationLink(value: .commonsVoteDetailView(vote: commonsVote)) {
+                    CommonsVoteRow(vote: commonsVote)
+                }
+            },
+            rowLoadingView: {
                 CommonsVoteRowLoading()
-            }
-        }
-        .listStyle(.grouped)
-        .environment(\.isScrollEnabled, false)
-    }
-
-    @ViewBuilder
-    var scrollView: some View {
-        List(viewModel.items) { vote in
-            ContextAwareNavigationLink(value: .commonsVoteDetailView(vote: vote)) {
-                CommonsVoteRow(vote: vote)
-                    .onAppear(perform: { onScrollEnd(vote: vote) })
-            }
-        }
-        .listStyle(.grouped)
-    }
-
-    private func onScrollEnd(vote: CommonsVote) {
-        if vote == viewModel.items.last {
-            viewModel.nextData()
-        }
+            },
+            navigationTitle: "Commons vote",
+            showNumResults: false
+        )
     }
 }
